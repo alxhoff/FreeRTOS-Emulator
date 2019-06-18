7ebdb559a9632f1c968e58d9badd2a52e4a49c99
Alex Hoffman <alxhoff@gmail.com>
2019-06-18 12:21:15 +0200

    More removed and restructured

---
+++ FreeRTOS_Posix/Build/Lib/Full/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Lib/Full/BlockQ.c \
+../Lib/Full/PollQ.c \
+../Lib/Full/death.c \
+../Lib/Full/dynamic.c \
+../Lib/Full/events.c \
+../Lib/Full/flash.c \
+../Lib/Full/integer.c \
+../Lib/Full/print.c \
+../Lib/Full/semtest.c 
+
+OBJS += \
+./Lib/Full/BlockQ.o \
+./Lib/Full/PollQ.o \
+./Lib/Full/death.o \
+./Lib/Full/dynamic.o \
+./Lib/Full/events.o \
+./Lib/Full/flash.o \
+./Lib/Full/integer.o \
+./Lib/Full/print.o \
+./Lib/Full/semtest.o 
+
+C_DEPS += \
+./Lib/Full/BlockQ.d \
+./Lib/Full/PollQ.d \
+./Lib/Full/death.d \
+./Lib/Full/dynamic.d \
+./Lib/Full/events.d \
+./Lib/Full/flash.d \
+./Lib/Full/integer.d \
+./Lib/Full/print.d \
+./Lib/Full/semtest.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Lib/Full/%.o: ../Lib/Full/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -D__GCC_POSIX__=1 -DDEBUG_BUILD=1 -DUSE_STDIO=1 -I../Lib/include -I.. -I../FreeRTOS_Kernel/include -I../FreeRTOS_Kernel/portable/GCC/Posix -O0 -g -Wall -c -fmessage-length=0 -pthread -lrt -Wno-pointer-sign -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ FreeRTOS_Posix/Build/Lib/Minimal/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Lib/Minimal/GenQTest.c \
+../Lib/Minimal/QPeek.c \
+../Lib/Minimal/blocktim.c \
+../Lib/Minimal/countsem.c \
+../Lib/Minimal/crflash.c \
+../Lib/Minimal/crhook.c \
+../Lib/Minimal/recmutex.c 
+
+OBJS += \
+./Lib/Minimal/GenQTest.o \
+./Lib/Minimal/QPeek.o \
+./Lib/Minimal/blocktim.o \
+./Lib/Minimal/countsem.o \
+./Lib/Minimal/crflash.o \
+./Lib/Minimal/crhook.o \
+./Lib/Minimal/recmutex.o 
+
+C_DEPS += \
+./Lib/Minimal/GenQTest.d \
+./Lib/Minimal/QPeek.d \
+./Lib/Minimal/blocktim.d \
+./Lib/Minimal/countsem.d \
+./Lib/Minimal/crflash.d \
+./Lib/Minimal/crhook.d \
+./Lib/Minimal/recmutex.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Lib/Minimal/%.o: ../Lib/Minimal/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -D__GCC_POSIX__=1 -DDEBUG_BUILD=1 -DUSE_STDIO=1 -I../Lib/include -I.. -I../FreeRTOS_Kernel/include -I../FreeRTOS_Kernel/portable/GCC/Posix -O0 -g -Wall -c -fmessage-length=0 -pthread -lrt -Wno-pointer-sign -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
