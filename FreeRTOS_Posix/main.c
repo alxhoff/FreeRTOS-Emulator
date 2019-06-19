1fa3af7eeee8630a611e6e60924e25eed49ad0ed
Alex Hoffman <alxhoff@gmail.com>
2019-06-19 09:47:59 +0200

    Some basic SDL added to test compatibility

--- FreeRTOS_Posix/main.c
+++ FreeRTOS_Posix/main.c
@@ -23,7 +23,20 @@
 #include "AsyncIO/AsyncIOSerial.h"
 
 #define mainGENERIC_PRIORITY	( tskIDLE_PRIORITY )
-#define mainGENERIC_STACK_SIZE  ( ( unsigned short ) 256 )
+#define mainGENERIC_STACK_SIZE  ( ( unsigned short ) 2560 )
+
+const int SCREEN_X = 100;
+const int SCREEN_Y = 200;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP = 32;
+
+/*
+ * Drawing
+ */
+SDL_Surface *screen = NULL;
+static void vInitDrawing( void );
+static void vDrawTask( void *pvParameters );
 
 /*
  * Demo task
@@ -32,34 +45,77 @@ static void vDemoTask1( void *pvParameters );
 
 int main( int argc, char *argv[] )
 {
-    SDL_Surface *hello = NULL;
-    SDL_Surface *screen = NULL;
+    vInitDrawing();
 
-    SDL_Init( SDL_INIT_EVERYTHING );
-
-    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
-
-    hello = SDL_LoadBMP("/home/alxhoff/git/GitHub/FreeRTOS-simulator-demo/FreeRTOS_Posix/Debug/test.bmp");
-
-    SDL_BlitSurface( hello, NULL, screen, NULL);
-
-    SDL_Flip(screen);
-
-    SDL_Delay(2000);
-
-    SDL_FreeSurface(hello);
-
-    /*
-     * Demo task
-     */
     xTaskCreate( vDemoTask1, "DemoTask1", mainGENERIC_STACK_SIZE, NULL, mainGENERIC_PRIORITY, NULL );
+    xTaskCreate( vDrawTask, "DrawTask", mainGENERIC_STACK_SIZE, NULL, mainGENERIC_STACK_SIZE, NULL);
 
-
-	/* Set the scheduler running.  This function will not return unless a task calls vTaskEndScheduler(). */
 	vTaskStartScheduler();
 
     SDL_Quit();
 	return 1;
+}
+
+void vInitDrawing( void )
+{
+    SDL_Init( SDL_INIT_EVERYTHING );
+
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+
+    SDL_WM_SetCaption("FreeRTOS Simulator", NULL);
+}
+
+void drawRect(int x, int y, int w, int h, SDL_Surface *screen)
+{
+    SDL_Rect rect;
+    
+}
+
+SDL_Surface *load_image(char *filename)
+{
+    SDL_Surface *loadedImage = NULL;
+    SDL_Surface *optimizedImage = NULL;
+
+    loadedImage = SDL_LoadBMP(filename);
+
+    if( loadedImage != NULL){
+        optimizedImage = SDL_DisplayFormat(loadedImage);
+
+        SDL_FreeSurface(loadedImage);
+    }
+
+    return optimizedImage;
+}
+
+void apply_surface( int x, int y, SDL_Surface *source, SDL_Surface *destination )
+{
+    SDL_Rect offset;
+
+    offset.x = x;
+    offset.y = y;
+
+    SDL_BlitSurface(source, NULL, destination, &offset);
+}
+
+void vDrawTask ( void *pvParameters )
+{
+    SDL_Surface *background = NULL;
+    SDL_Surface *hello = NULL;
+    
+    background = load_image("/home/alxhoff/git/GitHub/FreeRTOS-simulator-demo/FreeRTOS_Posix/Debug/background.bmp");
+    
+    hello = load_image("/home/alxhoff/git/GitHub/FreeRTOS-simulator-demo/FreeRTOS_Posix/Debug/test.bmp");
+
+    apply_surface(0,0,background,screen);
+    
+    while(1){
+
+        SDL_Flip(screen);
+
+        vTaskDelay(1000);
+    }
+        
+    SDL_FreeSurface(hello);
 }
 
 void vDemoTask1 ( void *pvParameters )
