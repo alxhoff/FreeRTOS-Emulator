a4696b0cf76a9d531544224ee9d1edbba810d7d6
Alex Hoffman <alxhoff@gmail.com>
2019-07-05 11:13:11 +0200

    Text added

--- FreeRTOS_Posix/main.c
+++ FreeRTOS_Posix/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <time.h>
 #include <sys/time.h>
 #include <stdlib.h>
@@ -13,6 +14,7 @@
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
 #include "SDL2/SDL_ttf.h"
+#include "SDL2/SDL2_gfxPrimitives.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -27,6 +29,13 @@
 #define mainGENERIC_PRIORITY	( tskIDLE_PRIORITY )
 #define mainGENERIC_STACK_SIZE  ( ( unsigned short ) 2560 )
 
+#define DEFAULT_FONT_SIZE   20
+
+typedef struct coord{
+    unsigned int x;
+    unsigned int y;
+}coord_t;
+
 const int SCREEN_X = 100;
 const int SCREEN_Y = 200;
 const int SCREEN_WIDTH = 640;
@@ -37,6 +46,8 @@ const int SCREEN_HEIGHT = 480;
  */
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+TTF_Font *font1 = NULL;
+SDL_Colour fontColour = {0,0,0,255};
 static void vInitDrawing( void );
 static void vDrawTask( void *pvParameters );
 
@@ -49,6 +60,11 @@ void logSDLError(char *msg)
 {
     if(msg)
         printf("[ERROR] %s, %s\n", msg, SDL_GetError());
+}
+
+void logTTFError(char *msg){
+    if(msg)
+        printf("[ERROR] %s, %s\n", msg, TTF_GetError());
 }
 
 int main( int argc, char *argv[] )
@@ -64,10 +80,52 @@ int main( int argc, char *argv[] )
 	return 1;
 }
 
+void vDrawCircle(int x, int y, int radius, int colour){
+    filledCircleColor(renderer, x, y, radius, colour | (0xFF << 24));
+}
+
+void vDrawLine(int x1, int y1, int x2, int y2, int colour){
+    lineColor(renderer, x1, y1, x2, y2, colour | (0xFF << 24)); 
+}
+
+void vDrawPoly(coord_t *points, unsigned int n, int colour)
+{
+    int16_t *x_coords = calloc(1, sizeof(int16_t) * n);
+    int16_t *y_coords = calloc(1, sizeof(int16_t) * n);
+
+    for(unsigned int i = 0; i < n; i++){
+        x_coords[i] = points[i].x;
+        y_coords[i] = points[i].y;
+    }
+
+    polygonColor(renderer, x_coords, y_coords, n, colour | (0xFF << 24));
+
+    free(x_coords);
+    free(y_coords);
+
+}
+
+void vDrawTriangle(coord_t *points, int colour){
+    filledTrigonColor(renderer, points[0].x, points[0].y, points[1].x, points[1].y,
+            points[2].x, points[2].y, colour | (0xFF << 24));
+}
+
 void vInitDrawing( void )
 {
+    int ret = 0;
+
     SDL_Init( SDL_INIT_EVERYTHING );
 
+    ret = TTF_Init();
+
+    if (ret == -1)
+        logTTFError("InitDrawing->Init");
+
+    font1 = TTF_OpenFont("fonts/IBMPlexSans-Medium.ttf", DEFAULT_FONT_SIZE);
+
+    if(!font1)
+        logTTFError("InitDrawing->OpenFont");
+    
     window = SDL_CreateWindow("FreeRTOS Simulator", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
     if (!window){
@@ -139,6 +197,28 @@ void vDrawClippedImage(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Re
     vDrawRectImage(tex, ren, dst, clip);
 }
 
+void vDrawText(char *string, int x, int y)
+{
+    SDL_Surface* solid = TTF_RenderText_Solid(font1, string, fontColour);
+
+    if(!solid)
+        logTTFError("DrawText->RenderText");
+
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, solid);
+
+    if(!tex)
+        logSDLError("DrawText->CreateTextureFromSurface");
+
+    SDL_FreeSurface(solid);
+    SDL_Rect rect = {.x = x,
+                    .y = y};
+
+    SDL_QueryTexture(tex, NULL, NULL, &rect.w, &rect.h);
+
+    SDL_RenderCopy(renderer, tex, NULL, &rect);
+
+}
+
 void vHandleSDLEvents(void)
 {
     SDL_Event e;
@@ -166,8 +246,12 @@ void vDrawTask ( void *pvParameters )
     
     vDrawImage(background, renderer, x, y);
 
+    vDrawCircle(100, 100, 50, 0x0000FF);
+
+    vDrawText("hello", 200, 200);
+
     SDL_RenderPresent(renderer);
-    SDL_Delay(1000);
+    SDL_Delay(2000);
 
     SDL_Quit();
     
