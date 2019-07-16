368a47e529de80773136c65d3f5d6989ba6c9f5d
Alex Hoffman <alxhoff@gmail.com>
2019-07-16 12:18:37 +0200

    Cmake written, needs fonts

---
+++ FreeRTOS_Posix/build/CMakeFiles/GFX.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alxhoff/git/GitHub/FreeRTOS-simulator-demo/FreeRTOS_Posix/lib/Gfx/TUM_Draw.c" "/home/alxhoff/git/GitHub/FreeRTOS-simulator-demo/FreeRTOS_Posix/build/CMakeFiles/GFX.dir/lib/Gfx/TUM_Draw.c.o"
+  "/home/alxhoff/git/GitHub/FreeRTOS-simulator-demo/FreeRTOS_Posix/lib/Gfx/TUM_Event.c" "/home/alxhoff/git/GitHub/FreeRTOS-simulator-demo/FreeRTOS_Posix/build/CMakeFiles/GFX.dir/lib/Gfx/TUM_Event.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GFX_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../lib/FreeRTOS_Kernel/include"
+  "../lib/FreeRTOS_Kernel/portable/GCC/Posix"
+  "../lib/AsyncIO/include"
+  "../include"
+  "../lib/Gfx/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FreeRTOS_Posix/build/CMakeFiles/GFX.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GFX.dir/lib/Gfx/TUM_Draw.c.o"
+  "CMakeFiles/GFX.dir/lib/Gfx/TUM_Event.c.o"
+  "libGFX.pdb"
+  "libGFX.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GFX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
