5cc7ef136967b12fcbb10852cc4754e9881f72c3
Alex Hoffman <alxhoff@gmail.com>
2020-04-02 21:51:37 +0200

    Added rolling average to smoothing FPS

--- src/main.c
+++ src/main.c
@@ -238,8 +238,14 @@ void vDrawHelpText(void)
 		  __FUNCTION__);
 }
 
+#define FPS_AVERAGE_COUNT 50
+
 void vDrawFPS(void)
 {
+    static unsigned int periods[FPS_AVERAGE_COUNT] = {0};
+    static unsigned int periods_total = 0;
+    static unsigned int index = 0;
+    static unsigned int average_count = 0;
 	static TickType_t xLastWakeTime = 0, prevWakeTime = 0;
 	static char str[10] = { 0 };
 	static int text_width;
@@ -248,11 +254,26 @@ void vDrawFPS(void)
 	xLastWakeTime = xTaskGetTickCount();
 
 	if (prevWakeTime != xLastWakeTime) {
-		fps = configTICK_RATE_HZ/(xLastWakeTime-prevWakeTime);
-	  prevWakeTime = xLastWakeTime;
+        periods[index] = configTICK_RATE_HZ/(xLastWakeTime-prevWakeTime);
+	    prevWakeTime = xLastWakeTime;
 	} else {
-		fps = 0;
+		periods[index] = 0;
 	}
+
+    periods_total += periods[index];
+
+    if(index == (FPS_AVERAGE_COUNT - 1))
+        index = 0;
+    else 
+        index++;
+
+    if(average_count < FPS_AVERAGE_COUNT)
+        average_count++;
+    else{
+        periods_total -= periods[index];
+    }
+
+    fps = periods_total / average_count;
 
 	sprintf(str, "FPS: %2d",fps);
 
