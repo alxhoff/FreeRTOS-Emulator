e9bd3a9deeb78ccca69330b8e24f1aaf48fe5786
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-03 08:44:59 +0000

    Deploy alxhoff/FreeRTOS-Emulator to github.com/alxhoff/FreeRTOS-Emulator.git:gh-pages

--- search/all_6.js
+++ search/all_6.js
@@ -12,6 +12,8 @@ var searchData=
   ['font_5flocation',['FONT_LOCATION',['../TUM__Draw_8h.html#a5c9e4dc2321c48920c739004f9a24766',1,'TUM_Draw.h']]],
   ['fonts_5flocation',['FONTS_LOCATION',['../TUM__Draw_8h.html#a08ed2d65a3da495e898051d718209dbf',1,'TUM_Draw.h']]],
   ['for_5feach_5fsample',['FOR_EACH_SAMPLE',['../group__tum__sound.html#ga9587246eddb1e135285b1f54a29b46c7',1,'TUM_Sound.h']]],
+  ['framelimit',['FRAMELIMIT',['../TUM__Draw_8c.html#a626982d1a22f6a6266238f1d4eb6411d',1,'TUM_Draw.c']]],
+  ['framelimit_5fperiod',['FRAMELIMIT_PERIOD',['../TUM__Draw_8c.html#aeef849eb53d39cc37e502ec6be795e5c',1,'TUM_Draw.c']]],
   ['fuchsia',['Fuchsia',['../group__tum__draw.html#ga44a688b3adc6060bb681f4aa8249bee3',1,'TUM_Draw.h']]],
   ['fullwavefilenames',['fullWaveFileNames',['../TUM__Sound_8c.html#a66b745d4db045c018199149441a1b773',1,'TUM_Sound.c']]],
   ['freertos_20simulator_20graphical_20library',['FreeRTOS Simulator Graphical Library',['../index.html',1,'']]]
--- search/defines_5.js
+++ search/defines_5.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['font_5flocation',['FONT_LOCATION',['../TUM__Draw_8h.html#a5c9e4dc2321c48920c739004f9a24766',1,'TUM_Draw.h']]],
-  ['fonts_5flocation',['FONTS_LOCATION',['../TUM__Draw_8h.html#a08ed2d65a3da495e898051d718209dbf',1,'TUM_Draw.h']]]
+  ['fonts_5flocation',['FONTS_LOCATION',['../TUM__Draw_8h.html#a08ed2d65a3da495e898051d718209dbf',1,'TUM_Draw.h']]],
+  ['framelimit',['FRAMELIMIT',['../TUM__Draw_8c.html#a626982d1a22f6a6266238f1d4eb6411d',1,'TUM_Draw.c']]],
+  ['framelimit_5fperiod',['FRAMELIMIT_PERIOD',['../TUM__Draw_8c.html#aeef849eb53d39cc37e502ec6be795e5c',1,'TUM_Draw.c']]]
 ];
