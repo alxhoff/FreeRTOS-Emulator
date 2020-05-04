30a28b193b6db9a9a2c12fc838b5c38d2562fa7c
PhilippvK <philipp.van-kempen@tum.de>
2020-05-04 12:30:53 +0200

    show fps in pong game

--- src/main.c
+++ src/main.c
@@ -322,56 +322,56 @@ void vDrawHelpText(void)
                   __FUNCTION__);
 }
 
-/** #define FPS_AVERAGE_COUNT 50 */
-/**  */
-/** void vDrawFPS(void) */
-/** { */
-/**     static unsigned int periods[FPS_AVERAGE_COUNT] = { 0 }; */
-/**     static unsigned int periods_total = 0; */
-/**     static unsigned int index = 0; */
-/**     static unsigned int average_count = 0; */
-/**     static TickType_t xLastWakeTime = 0, prevWakeTime = 0; */
-/**     static char str[10] = { 0 }; */
-/**     static int text_width; */
-/**     int fps = 0; */
-/**  */
-/**     xLastWakeTime = xTaskGetTickCount(); */
-/**  */
-/**     if (prevWakeTime != xLastWakeTime) { */
-/**         periods[index] = */
-/**             configTICK_RATE_HZ / (xLastWakeTime - prevWakeTime); */
-/**         prevWakeTime = xLastWakeTime; */
-/**     } */
-/**     else { */
-/**         periods[index] = 0; */
-/**     } */
-/**  */
-/**     periods_total += periods[index]; */
-/**  */
-/**     if (index == (FPS_AVERAGE_COUNT - 1)) { */
-/**         index = 0; */
-/**     } */
-/**     else { */
-/**         index++; */
-/**     } */
-/**  */
-/**     if (average_count < FPS_AVERAGE_COUNT) { */
-/**         average_count++; */
-/**     } */
-/**     else { */
-/**         periods_total -= periods[index]; */
-/**     } */
-/**  */
-/**     fps = periods_total / average_count; */
-/**  */
-/**     sprintf(str, "FPS: %2d", fps); */
-/**  */
-/**     if (!tumGetTextSize((char *)str, &text_width, NULL)) */
-/**         checkDraw(tumDrawText(str, SCREEN_WIDTH - text_width - 10, */
-/**                               SCREEN_HEIGHT - DEFAULT_FONT_SIZE * 1.5, */
-/**                               Blue), */
-/**                   __FUNCTION__); */
-/** } */
+#define FPS_AVERAGE_COUNT 50
+
+void vDrawFPS(void)
+{
+    static unsigned int periods[FPS_AVERAGE_COUNT] = { 0 };
+    static unsigned int periods_total = 0;
+    static unsigned int index = 0;
+    static unsigned int average_count = 0;
+    static TickType_t xLastWakeTime = 0, prevWakeTime = 0;
+    static char str[10] = { 0 };
+    static int text_width;
+    int fps = 0;
+
+    xLastWakeTime = xTaskGetTickCount();
+
+    if (prevWakeTime != xLastWakeTime) {
+        periods[index] =
+            configTICK_RATE_HZ / (xLastWakeTime - prevWakeTime);
+        prevWakeTime = xLastWakeTime;
+    }
+    else {
+        periods[index] = 0;
+    }
+
+    periods_total += periods[index];
+
+    if (index == (FPS_AVERAGE_COUNT - 1)) {
+        index = 0;
+    }
+    else {
+        index++;
+    }
+
+    if (average_count < FPS_AVERAGE_COUNT) {
+        average_count++;
+    }
+    else {
+        periods_total -= periods[index];
+    }
+
+    fps = periods_total / average_count;
+
+    sprintf(str, "FPS: %2d", fps);
+
+    if (!tumGetTextSize((char *)str, &text_width, NULL))
+        checkDraw(tumDrawText(str, (SCREEN_WIDTH / 2) - (text_width / 2),
+                              SCREEN_HEIGHT - DEFAULT_FONT_SIZE * 1.5,
+                              Blue),
+                  __FUNCTION__);
+}
 
 void vDrawWall(wall_t *wall)
 {
@@ -673,6 +673,7 @@ void vPongControlTask(void *pvParameters)
                                         my_ball->radius,
                                         my_ball->colour),
                           __FUNCTION__);
+                vDrawFPS();
 
                 xSemaphoreGive(ScreenLock);
 
