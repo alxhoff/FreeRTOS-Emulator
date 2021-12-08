a5f38884b6c8099df48690457209e5c7c62b65a1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-12-08 16:04:54 +0000

    Deploy alxhoff/FreeRTOS-Emulator to github.com/alxhoff/FreeRTOS-Emulator.git:gh-pages

--- search/typedefs_b.js
+++ search/typedefs_b.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['scaled_5fimage_5fdata_5ft',['scaled_image_data_t',['../TUM__Draw_8c.html#afa00c3abc81f7dbd5d021ecaf6519138',1,'TUM_Draw.c']]],
   ['sequence_5fhandle_5ft',['sequence_handle_t',['../group__tum__draw.html#ga1dafa9c3d2798f0f1ba06c637793b459',1,'TUM_Draw.h']]],
+  ['spritesheet_5fhandle_5ft',['spritesheet_handle_t',['../group__tum__draw.html#ga417cc5c34131017cfbbec75fb42af6c1',1,'TUM_Draw.h']]],
   ['spritesheet_5fsequence_5ft',['spritesheet_sequence_t',['../TUM__Draw_8c.html#aae8b3b09535ae980cbad2e464e7acffd',1,'TUM_Draw.c']]],
   ['spritesheet_5ft',['spritesheet_t',['../TUM__Draw_8c.html#a72eb6e3da47d5db1aeaf74873c783ed2',1,'TUM_Draw.c']]]
 ];
